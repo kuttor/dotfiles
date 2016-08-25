de91b8fbe97c012791e5a69bb3f4069d189f5329
Andrew Kuttor <andrew.kuttor@gmail.com>
2016-08-24 22:58:03 -0700

    added more zplug stuff

--- install.sh
+++ install.sh
@@ -18,3 +18,16 @@ ln -sf $DOTFILES/exports             ~/.exports
 #git clone https://github.com/github/hub.git ~/Downloads/hub
 #~/Downloads/script/build -o ~/.zsh/hub
 #eval "$(hub alias -s)"
+
+# install zplug and plugins
+git clone https://github.com/b4b4r07/zplug ~/.zplug
+source ~/.zplug/init.zsh            
+zplug load
+zplug "zsh-users/zsh-syntax-highlighting", nice:10
+zplug "zsh-users/zsh-history-substring-search"
+zplug "Jxck/dotfiles", as:command, use:"bin/{histuniq,color}"
+zplug "plugins/git",   from:oh-my-zsh
+zplug "b4b4r07/enhancd", use:init.sh
+zplug load --verbose
+zplug install
+~                                             
