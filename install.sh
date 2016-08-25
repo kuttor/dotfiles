c3693e2c89e035dacca777c1bd60ec9b5bce354f
Andrew Kuttor <akuttor@localhost.localdomain>
2016-08-24 22:43:27 -0700

    reworked - added zplug

--- install.sh
+++ install.sh
@@ -3,9 +3,18 @@ DOTFILES=`pwd`
 touch $DOTFILES/.private-gitconfig
 
 echo 'Create $HOME symlink'
-ln -sf $DOTFILES/.aliases             ~/.aliases
-ln -sf $DOTFILES/.sources             ~/.sources
-ln -sf $DOTFILES/.completions         ~/.completions
-ln -sf $DOTFILES/.functions           ~/.functions
-ln -sf $DOTFILES/.setops              ~/.setops
-ln -sf $DOTFILES/.sources             ~/.sources
+ln -sf $DOTFILES/aliases             ~/.aliases
+ln -sf $DOTFILES/sources             ~/.sources
+# ln -sf $DOTFILES/.completions      ~/.completions
+ln -sf $DOTFILES/functions           ~/.functions
+ln -sf $DOTFILES/setops              ~/.setops
+ln -sf $DOTFILES/sources             ~/.sources
+ln -sf $DOTFILES/exports             ~/.exports
+
+
+# installs for packages
+
+# hub                                                                                 
+#git clone https://github.com/github/hub.git ~/Downloads/hub
+#~/Downloads/script/build -o ~/.zsh/hub
+#eval "$(hub alias -s)"
