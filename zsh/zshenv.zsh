dc15b8576e41212d9e9a55b93c21c7e7ac675204
Andrew <andrew_kuttor@intuit.com>
2017-03-21 21:47:21 -0700

    corrected deprecated ZPlug key names

---
+++ zsh/zshenv.zsh
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+
+# Essential Env Vars
+export ZDOTDIR="$HOME/.zsh"
+export ZPLUG_HOME="$HOME/.zplug"
+export DOTFILES="$HOME/.dotfiles"
+
+# Source main user Zsh config
+source "$HOME/.zshrc"
