423aee5ba9369ae335e8fc4e0420a8ca969d0976
Andrew Kuttor <andrew.kuttor@gmail.com>
2017-07-08 08:23:28 -0700

    add installer for packages

---
+++ package_installer.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+# for Debian
+
+# Better df
+sudo apt install pydf
+
+# Better du
+sudo apt install ncdu
+
+# Better top
+sudo apt install htop
+
+# wget
+sudo apt install wget
+
+# curl
+sudo apt install curl
+
+# tree
+sudo apt install tree
+
