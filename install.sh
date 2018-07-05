681655da39fbe7c8c5b9a2688df0280b53cac03b
Andrew Kuttor <yeti@andys-mbp.attlocal.net>
2018-07-05 03:17:28 -0700

    V2!

---
+++ install.sh
@@ -0,0 +1,23 @@
+#! /usr/local/bin/bash
+
+#
+# Name: Andrew 'Yeti' Kuttor
+# Mail: andrew.kuttor@gmail.com
+# Info: Installs and configures dotfiles
+#
+
+# Exa: Modern LS
+brew install exa
+
+# RipGrep: Modern Grep
+brew install ripgrep
+
+# Hub: Git additions
+brew install hub
+
+# Bash-Git-Prompt: Sexy Prompt
+git clone \
+  https://github.com/magicmonty/bash-git-prompt.git \
+  .bash-git-prompt \
+  --depth=1
+
