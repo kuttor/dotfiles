2a6b0da0e69ebd49516b1dccd42d26984768f46b
Andrew Kuttor <andrew.kuttor@gmail.com>
2019-02-01 09:42:37 -0800

    fixed

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -19,13 +19,3 @@ export HISTCONTROL="ignoredups:erasedups"            # Ignore lead-space & dupes
 export HISTTIMEFORMAT="[%F %T] "                     # Custom time format
 export HISTIGNORE="ls:cd:exa:pwd:exit:history:clear" # Ignore recording commands
 
-# zsh-history-enhanced settings
-ZSH_HISTORY_FILTER="fzf:percol"
-ZSH_HISTORY_KEYBIND_GET_BY_DIR="^r"
-ZSH_HISTORY_KEYBIND_GET_ALL="^r^a"
-
-# default to `$HOME/.zsh_history`
-export ZSH_EXTEND_HISTORY_FILE="$HOME/.zsh_history"
-
-# print history collected on stdout instead of file
-#export ZSH_EXTEND_HISTORY_DEBUG="true"
