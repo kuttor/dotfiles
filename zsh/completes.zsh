d861330febe7cd5f8ba968827bbca69163fba649
Andrew Kuttor <andrew.kuttor@gmail.com>
2019-02-06 22:37:17 -0800

    Added more completes

--- zsh/completes.zsh
+++ zsh/completes.zsh
@@ -12,7 +12,7 @@ bashcompinit
 
 zmodload -i zsh/complist
 
-# The following lines were added by compinstall
+zstyle ':compinstall' filename '$HOME/.dotfiles/complete.zsh'
 zstyle ':completion:*' add-space true
 zstyle ':completion:*' auto-description 'Specify %d'
 zstyle ':completion:*' completer _list _oldlist _expand _complete _ignored _match _correct _approximate _prefix
@@ -20,18 +20,22 @@ zstyle ':completion:*' format 'Completing %d'
 zstyle ':completion:*' group-name ''
 zstyle ':completion:*' insert-unambiguous true
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*' list-colors ''
+zstyle ':completion:*' list-dirs-first true
 zstyle ':completion:*' list-prompt '%SAt %p: Hit TAB for more, or the character to insert%s'
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|=*' 'l:|=* r:|=*'
 zstyle ':completion:*' menu select=1
 zstyle ':completion:*' old-list always
 zstyle ':completion:*' old-menu false
 zstyle ':completion:*' original true
+zstyle ':completion:*' rehash true
 zstyle ':completion:*' select-prompt '%SScrolling active: current selection at %p%s'
 zstyle ':completion:*' special-dirs true
-zstyle ':completion:*:manuals' separate-sections true
 zstyle ':completion:*' verbose true
-zstyle :compinstall filename '$HOME/.dotfiles/complete.zsh'
-
-# End of lines added by compinstall
+zstyle ':completion:*:*:*:*:*' menu select
+zstyle ':completion:*:*:*:*:processes' command "ps -u $USER -o pid,user,comm -w -w"
+zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
+zstyle ':completion:*:manuals' separate-sections true
 
 # make autocompletion faster by caching and prefix-only matching
 zstyle ':completion:*' accept-exact '*(N)'
@@ -57,5 +61,9 @@ zstyle ':filter-select:highlight' matched fg=red
 zstyle ':filter-select' max-lines 10
 zstyle ':filter-select' rotate-list yes
 zstyle ':filter-select' case-insensitive yes # enable case-insensitive search
+
+
+
+
 
 autoload -Uz compinit && compinit
