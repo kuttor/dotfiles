871101c0ed113657ec16e7a144ef25a03a15337d
Andrew Kuttor <andrew.kuttor@gmail.com>
2019-02-06 22:45:28 -0800

    Added ls stuff

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -1,21 +1,46 @@
 #!/usr/local/bin/zsh
-# ------------------------------------------------------------------------------
+#
 # file  : history.zsh
 # info  : Extended history configurations for dotfiles
 # name  : Andrew Kuttor
 # mail  : andrew.kuttor@gmail.com
-# ------------------------------------------------------------------------------
 
-# history
-setopt APPENDHISTORY
+# -----------------------------------------------------------------------------
+# Options
+# -----------------------------------------------------------------------------
 
-# aliases
+setopt append_history
+setopt extended_history
+setopt hist_expire_dups_first
+setopt hist_ignore_dups
+setopt hist_ignore_space
+setopt hist_verify
+setopt inc_append_history
+setopt share_history
+setopt interactivecomments
+
+# Aliases ----------------------------------------------------------------------
 alias h="history"
 
-# export
-export HISTFILE="$HOME/.zsh_history"                 # History file name
-export HISTSIZE=100000                               # History file size
-export HISTCONTROL="ignoredups:erasedups"            # Ignore lead-space & dupes
-export HISTTIMEFORMAT="[%F %T] "                     # Custom time format
-export HISTIGNORE="ls:cd:exa:pwd:exit:history:clear" # Ignore recording commands
+# -----------------------------------------------------------------------------
+# Environmet Variables
+# -----------------------------------------------------------------------------
+
+# Filename
+[[ -z "$HISTFILE" ]] && HISTFILE=$CACHE/.zsh_history
+
+# Limits
+HISTSIZE=10000                               # History file size
+SAVEHIST=10000
+
+# Add timestamps
+case $HIST_STAMPS in
+  "mm/dd/yyyy") alias history='fc -fl 1' ;;
+  "dd.mm.yyyy") alias history='fc -El 1' ;;
+  "yyyy-mm-dd") alias history='fc -il 1' ;;
+  *) alias history='fc -l 1' ;;
+esac
+
+
+
 
--- zsh/zplug.zsh
+++ zsh/zplug.zsh
@@ -6,61 +6,58 @@
 # Mail: andrew.kuttor@gmail.com
 # -----------------------------------------------------------------------------
 
+# Essential
+source $ZPLUG_HOME/init.zsh
+
 # SELF MANAGE
-zplug "zplug/zplug", hook-build:"zplug --self-manage"
+# zplug "zplug/zplug", hook-build:"zplug --self-manage"
 
 # PROMPT
 zplug "mafredri/zsh-async", from:github
 zplug "sindresorhus/pure", use:pure.zsh, from:github, as:theme
 
-# MISC
+# # MISC
 zplug "b4b4r07/enhancd", use:init.sh
-zplug "hlissner/zsh-autopair", from:github, as:"plugin"
-zplug "knu/zsh-manydots-magic", from:github
-zplug "plugins/brew", from:oh-my-zsh
-zplug "plugins/chucknorris", from:oh-my-zsh
-zplug "plugins/dircycle", from:oh-my-zsh
-zplug "plugins/dirhistory", from:oh-my-zsh
-zplug "plugins/last-working-dir", from:oh-my-zsh
 zplug "plugins/osx", from:oh-my-zsh
 zplug "plugins/pip", from:oh-my-zsh
 zplug "plugins/sudo", from:oh-my-zsh
-zplug "plugins/virtualenvwrapper", from:oh-my-zsh
-zplug "zsh-users/zsh-completions", from:github
-zplug "lib/completion", from:oh-my-zsh, defer:0
 zplug "plugins/colored-man-pages", from:oh-my-zsh
-zplug "djui/alias-tips"
+zplug "hlissner/zsh-autopair"
+zplug "knu/zsh-manydots-magic"
 
 # COMPLETION
-zplug "plugins/compleat", from:oh-my-zsh
 zplug "plugins/completion", from:oh-my-zsh
-zplug "glidenote/hub-zsh-completion"
-zplug "Valodim/zsh-curl-completion"
+zplug "zsh-users/zsh-completions", from:github
 
 # GIT
-zplug "plugins/git", from:oh-my-zsh
 zplug "plugins/git-extras", from:oh-my-zsh
-zplug "plugins/git_remote_branch", from:oh-my-zsh
-zplug "plugins/gitfast", from:oh-my-zsh
 
-# 
+# Fuzzy
+zplug "rupa/z", use:z.sh
 zplug "skywind3000/z.lua"
 zplug "aperezdc/zsh-fzy"
-zplug "changyuheng/fz", from:github, defer:"1"
+zplug "junegunn/fzf", as:command, hook-build:"./install --bin", use:"bin/{fzf-tmux,fzf}"
 zplug "andrewferrier/fzf-z", from:github
 
-# HISTORY
-zplug "plugins/history", from:oh-my-zsh
-zplug "b4b4r07/zsh-history-enhanced", from:github
-zplug "zsh-users/zsh-history-substring-search", from:github, defer:3
+# theme
+[[ $TERM == "dumb" ]] && PS1='$ '
+
+# Pure
+zplug "sindresorhus/pure", use:pure.zsh, from:github, as:theme, \
+      if:"[[ $TERM != 'dumb' ]]", \
+      hook-load:"PURE_GIT_PULL=1"
+
+# Zsh-Async
+zplug "mafredri/zsh-async", from:github, on:"sindresorhus/pure"
+
+# Zsh-Autosuggestions
+zplug "zsh-users/zsh-autosuggestions", from:github
 
 # COLORS
-zplug "zdharma/fast-syntax-highlighting", hook-load:'FAST_HIGHLIGHT=()'
-zplug "zsh-users/zsh-autosuggestions", from:github, defer:2
+zplug "zsh-users/zsh-syntax-highlighting", defer:3
 
-# Install plugins if there are plugins that have not been installed
-if ! zplug check --verbose
-then
+# Install packages that have not been installed yet
+if ! zplug check --verbose; then
   zplug install
 fi
 
