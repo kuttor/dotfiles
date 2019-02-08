abe682862af76a559f05e2f348477597fab52185
Andrew Kuttor <andrew.kuttor@gmail.com>
2019-02-08 06:32:21 -0800

    added openconnect.sh

--- install.sh
+++ install.sh
@@ -6,3 +6,6 @@ ln -s /Users/$(whoami)/.dotfiles/gitignore /Users/$(whoami)/.gitignore
 
 mkdir $HOME/.config/bitbar-plugins
 ln -s /Users/$(whoami)/.dotfiles/openconnect.sh /Users/$(whoami)/.config/bitbar-plugins/openconnect.sh
+
+sudo echo "$(whoami) ALL=(ALL) NOPASSWD: /usr/local/bin/openconnect" >> /etc/sudoers
+sudo echo "$(whoami) ALL=(ALL) NOPASSWD: /usr/bin/killall -2 openconnect.sh" >> /etc/sudoers
