d8446ebd6384fecc5a26bbc8baba29ea4daea02d
Andrew Kuttor <andrew.kuttor@gmail.com>
2019-02-10 12:24:18 -0800

    changed name from keybindings to zkbd

---
+++ zsh/zkbd.zsh
@@ -0,0 +1,16 @@
+#!/usr/local/bin/zsh
+
+# Emacs keybindings
+bindkey -e
+
+# Edit the current command line in $EDITOR
+autoload -U edit-command-line
+zle -N edit-command-line
+bindkey '^E' edit-command-line
+
+# Space does history expansion
+bindkey ' ' magic-space
+
+# FN + Arrow keys
+bindkey "^[[H" beginning-of-line
+bindkey "^[[F" end-of-line
