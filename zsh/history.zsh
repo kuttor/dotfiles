6573fe87c93b3bda043463ad5efdde723e041f05
Andrew Kuttor <andrew.kuttor@gmail.com>
2019-02-12 22:54:01 -0800

    Added fctrl

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -12,11 +12,11 @@
 setopt append_history
 setopt extended_history
 setopt hist_expire_dups_first
+setopt hist_fcntl_lock
 setopt hist_ignore_dups
 setopt hist_ignore_space
 setopt hist_verify
 setopt share_history
-setopt interactivecomments
 
 # Aliases
 # -----------------------------------------------------------------------------
