1ccf06834331f2ccfc09e88490adaf89d9301fe5
Andrew Kuttor <andrew.kuttor@gmail.com>
2019-02-13 02:05:02 -0800

    added added hushlogin and vimrc.login

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
+# -----------------------------------------------------------------------------
+# file: install.sh
+# info: creates necessary links/files to setup dotfiles
+# -----------------------------------------------------------------------------
 
-ln -s /Users/$(whoami)/.dotfiles/zshrc /Users/$(whoami)/.zshrc
-ln -s /Users/$(whoami)/.dotfiles/gitconfig /Users/$(whoami)/.gitconfig
-ln -s /Users/$(whoami)/.dotfiles/gitignore /Users/$(whoami)/.gitignore
+ln -s "/Users/$(whoami)/.dotfiles/gitconfig" "/Users/$(whoami)/.gitconfig"
+ln -s "/Users/$(whoami)/.dotfiles/gitignore" "/Users/$(whoami)/.gitignore"
+ln -s "/Users/$(whoami)/.dotfiles/vimrc.local" "/Users/$(whoami)/.vimrc.local"
 
-mkdir $HOME/.config/bitbar-plugins
-ln -s /Users/$(whoami)/.dotfiles/openconnect.sh /Users/$(whoami)/.config/bitbar-plugins/openconnect.sh
-
-sudo echo "$(whoami) ALL=(ALL) NOPASSWD: /usr/local/bin/openconnect" >> /etc/sudoers
-sudo echo "$(whoami) ALL=(ALL) NOPASSWD: /usr/bin/killall -2 openconnect.sh" >> /etc/sudoers
+touch "$HOME/.hushlogin"
