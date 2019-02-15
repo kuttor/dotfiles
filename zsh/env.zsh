0b219817ca76fdaed2b9f3eba03ce34a015b5700
Andrew Kuttor <andrew.kuttor@gmail.com>
2019-02-15 00:43:53 -0800

    Fixed  margin lines

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -8,7 +8,7 @@
 # -----------------------------------------------------------------------------
 
 # ENV VAR
-# ==============================================================================
+# -----------------------------------------------------------------------------
 export DOTFILES="$HOME/.dotfiles"
 export ZPLUG_HOME="/usr/local/opt/zplug"
 export ZSH_FUNCTIONS="$DOTFILES/functions"
@@ -18,24 +18,25 @@ export TMUX="$HOME/.config/tmux"
 export TMUX_PLUGINS="$HOME/.config/tmux/plugins"
 
 # LANGUAGE
-# ==============================================================================
+# -----------------------------------------------------------------------------
 export LANGUAGE="en_US.UTF-8"
 export LANG="$LANGUAGE"
 export LC_ALL="$LANGUAGE"
 export LC_CTYPE="$LANGUAGE"
 
 # HOMEBREW
-# ==============================================================================
+# -----------------------------------------------------------------------------
 export HOMEBREW_GITHUB_API_TOKEN=aed27538de34dd4e7df7d5672c538f693f1109a0
 
 # Pager
-# ==============================================================================
+# -----------------------------------------------------------------------------
+export HOMEBREW_GITHUB_API_TOKEN=aed27538de34dd4e7df7d5672c538f693f1109a0
 export PAGER='bat'
 export MANPAGER='bat'
 export BAT_CONFIG_PATH="$DOTFILES/bat.conf"
 
 # TERMINAL
-# ==============================================================================
+# -----------------------------------------------------------------------------
 export KEYTIMEOUT=1
 export TERMINAL_DARK=1
 export _Z_DATA="$HOME/.config/z-data"
@@ -43,7 +44,7 @@ export ITERM_24BIT=1
 export WORDCHARS='*?-[]~\!#%^(){}<>|`@#%^*()+:?'
 
 # EDITOR
-# ==============================================================================
+# -----------------------------------------------------------------------------
 export EDITOR=$(which vim)
 export VISUAL="$EDITOR"
 export CVSEDITOR="$EDITOR"
@@ -51,22 +52,22 @@ export SVN_EDITOR="$EDITOR"
 export GIT_EDITOR="$EDITOR"
 
 # ENV
-# ==============================================================================
+# -----------------------------------------------------------------------------
 export RBENV_ROOT="$(brew --prefix rbenv)"
 export GEM_HOME="$(brew --prefix)/opt/gems"
 export GEM_PATH="$(brew --prefix)/opt/gems"
 
 # FZF
-# ==============================================================================
+# -----------------------------------------------------------------------------
 source /usr/local/opt/fzf/shell/key-bindings.zsh
 source /usr/local/opt/fzf/shell/completion.zsh
 FZF_DEFAULT_OPTS="--extended-exact --height 100% --cycle --no-reverse --bind ctrl-a:select-all "
 FZF_DEFAULT_OPTS+="--bind pgup:preview-up --bind pgdn:preview-down --bind ctrl-f:jump --bind ctrl-k:kill-line --bind ctrl-p:toggle-preview"
 FZF_DEFAULT_COMMAND='ag --nocolor -g ""'
 FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-
+  
 # z.lua
-# ==============================================================================
+# -----------------------------------------------------------------------------
 #eval "$(lua $ZPLUG_REPOS/skywind3000/z.lua/z.lua --init zsh)"
 _ZL_CMD="y"                               # command alias
 _ZL_DATA="$HOME/.config/zdatafile.lua"    # datafile location
@@ -74,13 +75,13 @@ _ZL_ECHO=1                                # Echo dirname after CD.
 _ZL_MATCH_MODE=1                          # Enable enhanced master.
 
 # Pager
-# ==============================================================================
+# -----------------------------------------------------------------------------
 export PAGER='bat'
 export MANPAGER='bat'
 export BAT_CONFIG_PATH="${DOTFILES}/bat.conf"
 
 # Enhancd
-# ==============================================================================
+# -----------------------------------------------------------------------------
 ENHANCD_FILTER="fzf --height 50% --reverse --ansi"
 ENHANCD_DOT_SHOW_FULLPATH=1
 ENHANCD_COMMAND="cdd"
@@ -88,5 +89,5 @@ ENHANCD_DIR="$HOME/.config/enhancd/"
 ENHANCD_DOT_SHOW_FULLPATH=1
 
 # MOLECULE
-# ==============================================================================
+# -----------------------------------------------------------------------------
 export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
