925d35979c70becc3e5b59737d9963c85d67cbb0
Andrew Kuttor <andrew.kuttor@gmail.com>
2019-03-30 11:58:43 -0700

    fixed

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -38,9 +38,6 @@ alias stripcolors='sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g"'
 alias myip='ifconfig -a | perl -nle"/(\d+\.\d+\.\d+\.\d+)/ && print $1"'
 alias set_bitbucket_user="git config user.name 'Andy Kuttor' && git config user.email 'akuttor@cis.ntt.com'"
 
-
-
-# Replace 'ls' with exa if it is available.
 if command -v exa >/dev/null 2>&1; then
     alias ls="exa --git --color=always --group-directories-first"
     alias ll="exa --all --long --git --color=automatic --group-directories-first"
