613e472d59ace24321a2e92926725502127e468f
Andrew Kuttor <andrew.kuttor@gmail.com>
2019-04-03 04:53:37 -0700

    attempt fix for autosugg

--- zsh/zplug.zsh
+++ zsh/zplug.zsh
@@ -1,4 +1,4 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
 # file: zplug.zsh
@@ -7,11 +7,9 @@
 # mail: andrew.kuttor@gmail.com
 
 # Check if zplug is installed
-if [[ ! -d "$(brew --prefix)/opt/zplug" ]]
-then
-brew install zplug
-  source "$ZPLUG_HOME/init.zsh"
-  zplug update --self
+if [[ ! -d "$(brew --prefix)/opt/zplug" ]]; then
+    brew install zplug
+    zplug update --self
 fi
 
 # Load Zplug
@@ -24,9 +22,9 @@ zplug "tysonwolker/iterm-tab-colors"
 zplug "changyuheng/zsh-interactive-cd"
 
 zplug "mafredri/zsh-async", \
-    from:"github",          \
-    use:"async.zsh",        \
-    hook-load:"async_init"
+from:"github",          \
+use:"async.zsh",        \
+hook-load:"async_init"
 
 zplug "unixorn/warhol.plugin.zsh"
 zplug "plugins/colored-man-pages", from:oh-my-zsh
@@ -37,25 +35,24 @@ zplug "github/hub", from:github
 zplug 'tevren/gitfast-zsh-plugin'
 zplug "plugins/pip", from:oh-my-zsh
 zplug "plugins/sudo", from:oh-my-zsh
-zplug "zsh-users/zsh-completions", from:github
 zplug "jimeh/zsh-peco-history", defer:2, hook-build:'ZSH_PECO_HISTORY_DEDUP=1'
 zplug "rupa/z", use:z.sh
 zplug "skywind3000/z.lua"
 zplug "aperezdc/zsh-fzy"
 
 zplug "junegunn/fzf",             \
-    as:command,                   \
-    hook-build:"./install --bin", \
-    use:"bin/{fzf-tmux,fzf}"
+as:command,                   \
+hook-build:"./install --bin", \
+use:"bin/{fzf-tmux,fzf}"
 
 zplug "andrewferrier/fzf-z"
-
 zplug "ytet5uy4/fzf-widgets", hook-load:'FZF_WIDGET_TMUX=1'
 
-# Magic
-zplug "zsh-users/zsh-autosuggestions", defer:2
-zplug "zsh-users/zsh-history-substring-search", defer:3
-zplug "zsh-users/zsh-syntax-highlighting", defer:3
+# zsh users
+zplug "zsh-users/zsh-completions",              defer:0
+zplug "zsh-users/zsh-autosuggestions",          defer:2, on:"zsh-users/zsh-completions"
+zplug "zsh-users/zsh-syntax-highlighting",      defer:3, on:"zsh-users/zsh-autosuggestions"
+zplug "zsh-users/zsh-history-substring-search", defer:3, on:"zsh-users/zsh-syntax-highlighting"
 
 # Check and install packages
 if ! zplug check --verbose
