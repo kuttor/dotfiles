9431fc8b671aee5e521eecf8e83f6c65174d7980
Andrew Kuttor <andrew.kuttor@gmail.com>
2019-04-03 04:55:49 -0700

    removed bad aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -60,9 +60,3 @@ do
 done
 
 
-Windows       : %APPDATA%\Code\User\settings.json
-Mac           :  $HOME/Library/Application Support/Code/User/settings.json
-Linux         :  $HOME/.config/Code/User/settings.json
-
-
-
