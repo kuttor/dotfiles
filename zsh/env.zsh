0f8a93861ba4f74f1a521e46236083e3d7a5d582
Andrew Kuttor <andrew.kuttor@cis.ntt.com>
2019-04-21 01:33:47 -0700

    fixed cleaned up

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -138,50 +138,29 @@ FZF_DEFAULT_OPTS+="
   --bind ctrl-p:toggle-preview
   --bind ctrl-a:select-all"
 
-#FZF_DEFAULT_COMMAND='ag --nocolor -g ""'
 FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-
-# fzf + ag configuration
-if _has fzf && _has ag
-then
-    export FZF_DEFAULT_COMMAND='ag --nocolor -g ""'
-    export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-    export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND"
-    export FZF_DEFAULT_OPTS='
-    --color fg:242,bg:236,hl:65
-    --color fg+:15,bg+:239,hl+:108
-    --color info:108,prompt:109
-    --color spinner:108,pointer:168,marker:168
-    '
-fi
-
-# fzf + ripgrep configuration
-if _has fzf && _has rg
-then
-    export FZF_DEFAULT_COMMAND='rg --files --hidden --follow -g "!{.git}" 2>/dev/null'
-    export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-    export FZF_DEFAULT_OPTS=''
-fi
+export FZF_DEFAULT_COMMAND='ag --nocolor -g ""'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_DEFAULT_OPTS='
+--color fg:242,bg:236,hl:65
+--color fg+:15,bg+:239,hl+:108
+--color info:108,prompt:109
+--color spinner:108,pointer:168,marker:168'
 
 # Add <TAB> completion handlers for fzf *after* fzf is loaded
-_fzf_complete_z() {
-    _fzf_complete '--multi --reverse' "$@" < <(raw_z)
-}
+_fzf_complete_z() { _fzf_complete '--multi --reverse' "$@" < <(raw_z) }
 
 # Use rg to generate file completions
-_fzf_compgen_path() {
-    rg --files "$1" | with-dir "$1"
-}
+_fzf_compgen_path() { rg --files "$1" | with-dir "$1" }
 
 # Use rg to generate the list for directory completion
-_fzf_compgen_dir() {
-    rg --files "$1" | only-dir "$1"
+_fzf_compgen_dir() { rg --files "$1" | only-dir "$1"i }
 
 # z.lua
 # -----------------------------------------------------------------------------
 #eval "$(lua $ZPLUG_REPOS/skywind3000/z.lua/z.lua --init zsh)"
-_ZL_CMD="y" # command alias
+_ZL_CMD="y"                      # command alias
 _ZL_DATA="$CONFIG/zdatafile.lua" # datafile location
-_ZL_ECHO=1 # Echo dirname after CD
-_ZL_MATCH_MODE=1 # Enable enhanced master
-
+_ZL_ECHO=1                       # Echo dirname after CD
+_ZL_MATCH_MODE=1                 # Enable enhanced master
