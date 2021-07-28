46b9f97d239afd7b7dd2c1d7c8ddc5883f46e40f
Andrew Kuttor <akuttor@Andrew-Ks-MacBook-Pro.local>
2021-07-28 03:08:41 -0700

    Pruning config

--- wsl/wsl-install.ps1
+++
@@ -1,8 +0,0 @@
-# enablee Windows Subsystem for Linux feature
-dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
-
-# Enablee Virtual Machine Platfomr
-dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
-
-# Set WSL 2 as default
-wsl --set-default-version 2
