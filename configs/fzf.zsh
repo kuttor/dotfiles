a7fd4b0f3d83c6adc34a4ccfbd0afa3037a75963
changeme <changeme>
2024-03-09 01:29:22 -0800

    Git completionds not working:wq!

--- configs/fzf.zsh
+++ configs/fzf.zsh
@@ -51,6 +51,21 @@ fd \
 --type=d \
 "
 
+# Print tree structure in the preview window
+export FZF_ALT_C_OPTS="--preview 'eza --tree -C {}'"
+
+export FZF_CTRL_T_OPTS="
+  --preview 'bat -n --color=always {}'
+  --bind 'ctrl-/:change-preview-window(down|hidden|)'"
+
+# CTRL-/ to toggle small preview window to see the full command
+# CTRL-Y to copy the command into clipboard using pbcopy
+export FZF_CTRL_R_OPTS="
+  --preview 'echo {}' --preview-window up:3:hidden:wrap
+  --bind 'ctrl-/:toggle-preview'
+  --bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'
+  --color header:italic
+  --header 'Press CTRL-Y to copy command into clipboard'"
 # ------------------------------------------------------------------------------
 # ~ Completions ~
 
