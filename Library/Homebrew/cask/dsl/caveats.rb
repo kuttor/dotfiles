a77a8ca5642b813322a8f98abdd5436c8bfd99e2
Bevan Kay <email@bevankay.me>
2024-07-09 15:42:38 +1000

    cask/dsl/caveats: allow for simulated system checks in rosetta caveat

--- Library/Homebrew/cask/dsl/caveats.rb
+++ Library/Homebrew/cask/dsl/caveats.rb
@@ -141,7 +141,7 @@ module Cask
       end
 
       caveat :requires_rosetta do
-        next unless Hardware::CPU.arm?
+        next if !Hardware::CPU.arm? && Homebrew::SimulateSystem.current_arch != :arm
 
         <<~EOS
           #{@cask} is built for Intel macOS and so requires Rosetta 2 to be installed.
