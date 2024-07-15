e0db9061d1ddec0e5d6e5e8604de17ee142cfb22
Ruoyu Zhong <zhongruoyu@outlook.com>
2024-07-15 11:39:50 -0400

    cmd/formulae: use `ShellCommand`

--- Library/Homebrew/cmd/formulae.rb
+++ Library/Homebrew/cmd/formulae.rb
@@ -2,16 +2,16 @@
 # frozen_string_literal: true
 
 require "abstract_command"
+require "shell_command"
 
 module Homebrew
   module Cmd
     class Formulae < AbstractCommand
+      include ShellCommand
+
       cmd_args do
         description "List all locally installable formulae including short names."
       end
-
-      sig { override.void }
-      def run = raise_sh_command_error!
     end
   end
 end
