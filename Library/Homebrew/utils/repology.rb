11d6785beab1eed09de63ab9b122d4f01b8566e0
Sam Ford <1584702+samford@users.noreply.github.com>
2024-07-15 13:44:01 -0400

    Add utils/backtrace requires
    This is primarily intended to resolve the `uninitialized constant
    Utils::Backtrace` error in `formula_versions.rb:60` but I expanded it
    to try to cover all existing usage of `Utils::Backtrace`.
    
    I've followed the existing pattern, where `utils/backtrace` is
    required in the context of where it's used. Many of these cases use
    `Backtrace` in a conditional manner, so I've tried to ensure that the
    `require` follows suit.

--- Library/Homebrew/utils/repology.rb
+++ Library/Homebrew/utils/repology.rb
@@ -36,6 +36,7 @@ module Repology
     data = JSON.parse(output)
     { name => data }
   rescue => e
+    require "utils/backtrace"
     error_output = [errors, "#{e.class}: #{e}", Utils::Backtrace.clean(e)].compact
     if Homebrew::EnvConfig.developer?
       $stderr.puts(*error_output)
