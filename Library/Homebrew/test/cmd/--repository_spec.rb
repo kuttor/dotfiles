38987be01fe4b5d40581fa6897e096f9fd260e46
Bo Anderson <mail@boanderson.me>
2024-07-25 01:42:53 +0100

    cmd/--repository: fix homebrew- prefix handling

--- Library/Homebrew/test/cmd/--repository_spec.rb
+++ Library/Homebrew/test/cmd/--repository_spec.rb
@@ -14,4 +14,11 @@ RSpec.describe "brew --repository", type: :system do
       .and not_to_output.to_stderr
       .and be_a_success
   end
+
+  it "prints a Tap's repository correctly when homebrew- prefix is supplied", :integration_test do
+    expect { brew_sh "--repository", "foo/homebrew-bar" }
+      .to output("#{ENV.fetch("HOMEBREW_LIBRARY")}/Taps/foo/homebrew-bar\n").to_stdout
+      .and not_to_output.to_stderr
+      .and be_a_success
+  end
 end
