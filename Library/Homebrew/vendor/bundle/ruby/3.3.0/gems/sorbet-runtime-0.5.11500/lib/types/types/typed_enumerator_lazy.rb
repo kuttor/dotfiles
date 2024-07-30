14553e367f6de528a26e77960c089f21f03e86b0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-30 16:42:50 +0000

    brew vendor-gems: commit updates.

--- Library/Homebrew/vendor/bundle/ruby/3.3.0/gems/sorbet-runtime-0.5.11500/lib/types/types/typed_enumerator_lazy.rb
+++ Library/Homebrew/vendor/bundle/ruby/3.3.0/gems/sorbet-runtime-0.5.11500/lib/types/types/typed_enumerator_lazy.rb
@@ -2,37 +2,37 @@
 # typed: true
 
 module T::Types
-  class TypedEnumeratorChain < TypedEnumerable
+  class TypedEnumeratorLazy < TypedEnumerable
     def underlying_class
-      Enumerator::Chain
+      Enumerator::Lazy
     end
 
     # overrides Base
     def name
-      "T::Enumerator::Chain[#{type.name}]"
+      "T::Enumerator::Lazy[#{type.name}]"
     end
 
     # overrides Base
     def recursively_valid?(obj)
-      obj.is_a?(Enumerator::Chain) && super
+      obj.is_a?(Enumerator::Lazy) && super
     end
 
     # overrides Base
     def valid?(obj)
-      obj.is_a?(Enumerator::Chain)
+      obj.is_a?(Enumerator::Lazy)
     end
 
     def new(*args, &blk)
-      T.unsafe(Enumerator::Chain).new(*args, &blk)
+      T.unsafe(Enumerator::Lazy).new(*args, &blk)
     end
 
-    class Untyped < TypedEnumeratorChain
+    class Untyped < TypedEnumeratorLazy
       def initialize
-        super(T.untyped)
+        super(T::Types::Untyped::Private::INSTANCE)
       end
 
       def valid?(obj)
-        obj.is_a?(Enumerator::Chain)
+        obj.is_a?(Enumerator::Lazy)
       end
     end
   end
