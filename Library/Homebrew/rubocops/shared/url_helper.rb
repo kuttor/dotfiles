f036c2cc1d591875dd666fcd77da8971a0f76f19
Klaus Hipp <khipp@users.noreply.github.com>
2024-08-02 20:00:10 +0200

    rubocops/shared/url_helper: update url audit

--- Library/Homebrew/rubocops/shared/url_helper.rb
+++ Library/Homebrew/rubocops/shared/url_helper.rb
@@ -241,7 +241,6 @@ module RuboCop
         audit_urls(urls, zip_gh_pattern) do |_, url|
           next if url.match? %r{raw.githubusercontent.com/.*/.*/(main|master|HEAD)/}
           next if url.include?("releases/download")
-          next if url.include?("desktop.githubusercontent.com/github-desktop/releases/")
           next if url.include?("desktop.githubusercontent.com/releases/")
 
           problem "Use GitHub tarballs rather than zipballs (url is #{url})."
