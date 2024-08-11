52b25d71f5aaedb7f61f2c8389830b124b6cab67
Douglas Eichelberger <dduugg@gmail.com>
2024-08-11 16:56:20 -0700

    Make Cask::Artifact::Installer typesafe

--- Library/Homebrew/cask/artifact/installer.rb
+++ Library/Homebrew/cask/artifact/installer.rb
@@ -13,22 +13,14 @@ module Cask
         :script,
       ]).freeze
 
-      # Extension module for manual installers.
-      module ManualInstaller
-        def install_phase(**)
+      def install_phase(command: nil, **_)
+        if manual_install
           puts <<~EOS
             Cask #{cask} only provides a manual installer. To run it and complete the installation:
               open #{cask.staged_path.join(path).to_s.shellescape}
           EOS
-        end
-      end
-
-      # Extension module for script installers.
-      module ScriptInstaller
-        def install_phase(command: nil, **_)
-          # TODO: The `T.unsafe` is a false positive that is unnecessary in newer releasese of Sorbet
-          # (confirmend with sorbet v0.5.10672)
-          ohai "Running #{T.unsafe(self.class).dsl_key} script '#{path}'"
+        else
+          ohai "Running #{self.class.dsl_key} script '#{path}'"
 
           executable_path = staged_path_join_executable(path)
 
@@ -69,32 +61,33 @@ module Cask
 
       attr_reader :path, :args
 
+      sig { returns(T::Boolean) }
+      attr_reader :manual_install
+
       def initialize(cask, **args)
         super
 
         if args.key?(:manual)
           @path = Pathname(args[:manual])
           @args = []
-          extend(ManualInstaller)
-          return
+          @manual_install = true
+        else
+          path, @args = self.class.read_script_arguments(
+            args[:script], self.class.dsl_key.to_s, { must_succeed: true, sudo: false }, print_stdout: true
+          )
+          raise CaskInvalidError.new(cask, "#{self.class.dsl_key} missing executable") if path.nil?
+
+          @path = Pathname(path)
+          @manual_install = false
         end
-
-        path, @args = self.class.read_script_arguments(
-          args[:script], self.class.dsl_key.to_s, { must_succeed: true, sudo: false }, print_stdout: true
-        )
-        raise CaskInvalidError.new(cask, "#{self.class.dsl_key} missing executable") if path.nil?
-
-        @path = Pathname(path)
-        extend(ScriptInstaller)
       end
 
-      def summarize
-        path.to_s
-      end
+      sig { override.returns(String) }
+      def summarize = path.to_s
 
       def to_h
         { path: }.tap do |h|
-          h[:args] = args unless is_a?(ManualInstaller)
+          h[:args] = args unless manual_install
         end
       end
     end
