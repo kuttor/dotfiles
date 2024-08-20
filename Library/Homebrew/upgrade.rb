e91ed65aef7ff04565fb6bd589f152a54965be56
Bo Anderson <mail@boanderson.me>
2024-08-20 15:33:08 +0100

    upgrade: remove `module_function`

--- Library/Homebrew/upgrade.rb
+++ Library/Homebrew/upgrade.rb
@@ -11,9 +11,7 @@ require "utils/topological_hash"
 module Homebrew
   # Helper functions for upgrading formulae.
   module Upgrade
-    module_function
-
-    def upgrade_formulae(
+    def self.upgrade_formulae(
       formulae_to_install,
       flags:,
       dry_run: false,
@@ -97,13 +95,13 @@ module Homebrew
       end
     end
 
-    def outdated_kegs(formula)
+    private_class_method def self.outdated_kegs(formula)
       [formula, *formula.old_installed_formulae].map(&:linked_keg)
                                                 .select(&:directory?)
                                                 .map { |k| Keg.new(k.resolved_path) }
     end
 
-    def print_upgrade_message(formula, fi_options)
+    private_class_method def self.print_upgrade_message(formula, fi_options)
       version_upgrade = if formula.optlinked?
         "#{Keg.new(formula.opt_prefix).version} -> #{formula.pkg_version}"
       else
@@ -113,7 +111,7 @@ module Homebrew
       puts "  #{version_upgrade} #{fi_options.to_a.join(" ")}"
     end
 
-    def create_formula_installer(
+    private_class_method def self.create_formula_installer(
       formula,
       flags:,
       installed_on_request: false,
@@ -165,9 +163,8 @@ module Homebrew
         }.compact,
       )
     end
-    private_class_method :create_formula_installer
 
-    def upgrade_formula(formula_installer, dry_run: false, verbose: false)
+    def self.upgrade_formula(formula_installer, dry_run: false, verbose: false)
       formula = formula_installer.formula
 
       if dry_run
@@ -188,9 +185,8 @@ module Homebrew
       puts
       Homebrew.failed = true
     end
-    private_class_method :upgrade_formula
 
-    def install_formula(formula_installer, upgrade:)
+    def self.install_formula(formula_installer, upgrade:)
       formula = formula_installer.formula
 
       formula_installer.check_installation_already_attempted
@@ -224,7 +220,7 @@ module Homebrew
       end
     end
 
-    def check_broken_dependents(installed_formulae)
+    private_class_method def self.check_broken_dependents(installed_formulae)
       CacheStoreDatabase.use(:linkage) do |db|
         installed_formulae.flat_map(&:runtime_installed_formula_dependents)
                           .uniq
@@ -249,7 +245,7 @@ module Homebrew
       @puts_no_installed_dependents_check_disable_message_if_not_already = true
     end
 
-    def check_installed_dependents(
+    def self.check_installed_dependents(
       formulae,
       flags:,
       dry_run: false,
@@ -435,7 +431,7 @@ module Homebrew
       end
     end
 
-    def depends_on(one, two)
+    private_class_method def self.depends_on(one, two)
       if one.any_installed_keg
             &.runtime_dependencies
             &.any? { |dependency| dependency["full_name"] == two.full_name }
@@ -444,6 +440,5 @@ module Homebrew
         one <=> two
       end
     end
-    private_class_method :depends_on
   end
 end
