4ab19e09da12b8510caa3698ca8c9e1941507ed2
Francois-Xavier Coudert <fxcoudert@gmail.com>
2024-09-09 21:34:20 +0200

    Make Sequoia supported

--- Library/Homebrew/brew.sh
+++ Library/Homebrew/brew.sh
@@ -516,13 +516,13 @@ esac
 # - https://github.com/Homebrew/install/blob/HEAD/install.sh
 # and, if needed:
 # - MacOSVersion::SYMBOLS
-HOMEBREW_MACOS_NEWEST_UNSUPPORTED="15"
+HOMEBREW_MACOS_NEWEST_UNSUPPORTED="16"
 # TODO: bump version when new macOS is released and update references in:
 # - docs/Installation.md
 # - HOMEBREW_MACOS_OLDEST_SUPPORTED in .github/workflows/pkg-installer.yml
 # - `os-version min` in package/Distribution.xml
 # - https://github.com/Homebrew/install/blob/HEAD/install.sh
-HOMEBREW_MACOS_OLDEST_SUPPORTED="12"
+HOMEBREW_MACOS_OLDEST_SUPPORTED="13"
 HOMEBREW_MACOS_OLDEST_ALLOWED="10.11"
 
 if [[ -n "${HOMEBREW_MACOS}" ]]
