401b45fb01aa75d6b766ef9931b9c0c65bc81098
Ruiyang Wu <ywwry66@outlook.com>
2024-09-21 22:28:24 -0400

    keg: Install `gzip`ed info files during linking
    Currently, `brew link` installs `*.info` files to
    `#{HOMEBREW_PREFIX}/share/info/dir` using the `install_info` method.
    However, some formulae (e.g., `Emacs`) also ship `*.info.gz` files,
    which are only `symlink`ed but not installed.
    
    This commit allows `*.info.gz` files to be installed during linking in
    addition to the `*.info` files.

--- Library/Homebrew/keg.rb
+++ Library/Homebrew/keg.rb
@@ -77,7 +77,7 @@ class Keg
 
   # Locale-specific directories have the form `language[_territory][.codeset][@modifier]`
   LOCALEDIR_RX = %r{(locale|man)/([a-z]{2}|C|POSIX)(_[A-Z]{2})?(\.[a-zA-Z\-0-9]+(@.+)?)?}
-  INFOFILE_RX = %r{info/([^.].*?\.info|dir)$}
+  INFOFILE_RX = %r{info/([^.].*?\.info(\.gz)?|dir)$}
   KEG_LINK_DIRECTORIES = %w[
     bin etc include lib sbin share var
   ].freeze
