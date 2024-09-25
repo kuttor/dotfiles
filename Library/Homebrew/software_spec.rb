lords-7/essai2

cmd/info: show size information

Library/Homebrew/software_spec.rb
@@ -388,10 +388,10 @@ class Bottle
     resource.downloader.stage
   end
 
-  def fetch_tab
+  def fetch_tab(quiet: false)
     return if github_packages_manifest_resource.blank?
 
-    github_packages_manifest_resource.fetch
+    github_packages_manifest_resource.fetch(quiet:)
   rescue DownloadError
     raise unless fallback_on_error
 
@@ -408,6 +408,20 @@ class Bottle
     return {} unless github_packages_manifest_resource&.downloaded?
 
     github_packages_manifest_resource.tab
+  end
+
+  sig { returns(Integer) }
+  def bottle_size
+    return 0 unless github_packages_manifest_resource&.downloaded?
+
+    github_packages_manifest_resource.bottle_size
+  end
+
+  sig { returns(Integer) }
+  def installed_size
+    return 0 unless github_packages_manifest_resource&.downloaded?
+
+    github_packages_manifest_resource.installed_size
   end
 
   sig { returns(Filename) }

kuttor/dotfiles

Co-authored-by: Mike McQuaid <mike@mikemcquaid.com>
Co-authored-by: Markus Reiter <me@reitermark.us>

Library/Homebrew/software_spec.rb
@@ -436,6 +436,22 @@ class Bottle
     {}
   end
 
+  sig { returns(T.nilable(Integer)) }
+  def bottle_size
+    resource = github_packages_manifest_resource
+    return unless resource&.downloaded?
+
+    resource.bottle_size
+  end
+
+  sig { returns(T.nilable(Integer)) }
+  def installed_size
+    resource = github_packages_manifest_resource
+    return unless resource&.downloaded?
+
+    resource.installed_size
+  end
+
   sig { returns(Filename) }
   def filename
     Filename.create(resource.owner, @tag, @spec.rebuild)