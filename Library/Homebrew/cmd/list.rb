4c60dc0b2cb390370c90826e05bd902f253af7ec
Michael Cho <michael@michaelcho.dev>
2024-09-26 11:58:33 -0400

    cmd/list: `-t` and `-r` support for some long options

--- Library/Homebrew/cmd/list.rb
+++ Library/Homebrew/cmd/list.rb
@@ -69,13 +69,14 @@ module Homebrew
           conflicts "--cask", flag
           conflicts "--versions", flag
           conflicts "--pinned", flag
+          conflicts "-l", flag
         end
         ["-1", "-l", "-r", "-t"].each do |flag|
           conflicts "--versions", flag
           conflicts "--pinned", flag
         end
         ["--versions", "--pinned",
-         "---installed-on-request", "--installed-as-dependency",
+         "--installed-on-request", "--installed-as-dependency",
          "--poured-from-bottle", "--built-from-source",
          "-l", "-r", "-t"].each do |flag|
           conflicts "--full-name", flag
@@ -122,7 +123,13 @@ module Homebrew
 
           raise UsageError, "Cannot use #{flags.join(", ")} with formula arguments." unless args.no_named?
 
-          Formula.installed.sort.each do |formula|
+          formulae = if args.t?
+            Formula.installed.sort_by { |formula| test("M", formula.rack) }.reverse!
+          else
+            Formula.installed.sort
+          end
+          formulae.reverse! if args.r?
+          formulae.each do |formula|
             tab = Tab.for_formula(formula)
 
             statuses = []
