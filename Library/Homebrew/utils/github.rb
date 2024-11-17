ba4dfb26640b5840bd67a6f470d7355d19214f17
Elijah Olmos <35435704+elijaholmos@users.noreply.github.com>
2024-11-17 15:06:34 -0700

    Update Library/Homebrew/utils/github.rb
    Co-authored-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Homebrew/utils/github.rb
+++ Library/Homebrew/utils/github.rb
@@ -944,7 +944,7 @@ module GitHub
       end
     rescue => e
       # Ignore SAML access errors (https://github.com/Homebrew/brew/issues/18610)
-      raise unless e.message.include?("SAML")
+      raise unless e.message.include?("Resource protected by organization SAML enforcement")
     end
 
     false
