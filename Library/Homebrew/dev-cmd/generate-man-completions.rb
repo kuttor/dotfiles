79fb75177f58fe9819442650992c8d9bc3b44e1e
Eric Knibbe <enk3@outlook.com>
2024-12-02 15:36:00 -0500

    generate-man-completions: also fail if only date changed

--- Library/Homebrew/dev-cmd/generate-man-completions.rb
+++ Library/Homebrew/dev-cmd/generate-man-completions.rb
@@ -28,10 +28,13 @@ module Homebrew
         Completions.update_shell_completions!
 
         diff = system_command "git", args: [
-          "-C", HOMEBREW_REPOSITORY, "diff", "--exit-code", "docs/Manpage.md", "manpages", "completions"
+          "-C", HOMEBREW_REPOSITORY,
+          "diff", "--shortstat", "--patch", "--exit-code", "docs/Manpage.md", "manpages", "completions"
         ]
         if diff.status.success?
           ofail "No changes to manpage or completions."
+        elsif /1 file changed, 1 insertion\(\+\), 1 deletion\(-\).*-\.TH "BREW" "1" "\w+ \d+"/m.match?(diff.stdout)
+          ofail "No changes to manpage or completions other than the date."
         else
           puts "Manpage and completions updated."
         end
