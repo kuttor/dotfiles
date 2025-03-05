817c40d2618e0e4aee902a7ef2e1e42520cab7a1
thibhero <thibaut.herault@gmail.com>
2025-03-05 17:37:25 -0500

    test for upgrade_spec.rb

--- Library/Homebrew/test/cmd/upgrade_spec.rb
+++ Library/Homebrew/test/cmd/upgrade_spec.rb
@@ -15,4 +15,17 @@ RSpec.describe Homebrew::Cmd::UpgradeCmd do
     expect(HOMEBREW_CELLAR/"testball/0.1").to be_a_directory
     expect(HOMEBREW_CELLAR/"testball/0.0.1").not_to exist
   end
+
+  it "upgrades with asking for user prompts", :integration_test do
+    setup_test_formula "testball"
+    (HOMEBREW_CELLAR/"testball/0.0.1/foo").mkpath
+
+    expect {
+      brew "upgrade", "--ask"
+    }.to output(/.*Formula\s*\(1\):\s*testball.*/
+         ).to_stdout.and not_to_output.to_stderr
+
+    expect(HOMEBREW_CELLAR/"testball/0.1").to be_a_directory
+    expect(HOMEBREW_CELLAR/"testball/0.0.1").not_to exist
+  end
 end
