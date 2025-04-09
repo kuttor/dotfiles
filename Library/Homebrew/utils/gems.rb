a272310c37569b7be6ad8a3d4a46f2c43e2f575f
Patrick Linnane <patrick@linnane.io>
2025-04-09 11:43:19 -0700

    utils/gems: update Bundler version
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Library/Homebrew/utils/gems.rb
+++ Library/Homebrew/utils/gems.rb
@@ -10,7 +10,7 @@ Homebrew::FastBootRequire.from_rubylibdir("English")
 module Homebrew
   # Keep in sync with the `Gemfile.lock`'s BUNDLED WITH.
   # After updating this, run `brew vendor-gems --update=--bundler`.
-  HOMEBREW_BUNDLER_VERSION = "2.5.20"
+  HOMEBREW_BUNDLER_VERSION = "2.5.22"
 
   # Bump this whenever a committed vendored gem is later added to or exclusion removed from gitignore.
   # This will trigger it to reinstall properly if `brew install-bundler-gems` needs it.
